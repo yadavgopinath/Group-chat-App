86013a08939b971a2126a02ac044a1478a06b858
yadavgopinath <yadavgopinath93@gmail.com>
2024-11-24 23:30:49 +0530

    login-backend

--- controller/user.js
+++ controller/user.js
@@ -1,6 +1,7 @@
 const Users = require('../models/user');
 const bcrypt = require('bcrypt');
 const Sequelize = require('sequelize');
+const jwt = require('jsonwebtoken');
 
 exports.addUsers =async (req,res,next)=>{
  try{
@@ -41,4 +42,51 @@ res.status(201).json({ message: 'User added successfully', user: newUser });
     console.log(err);
     res.status(500).json({ message: 'An error occurred', error: err });
  }
+}
+
+
+
+exports.generateAccessToken = function(id,name){
+   
+    return jwt.sign({userId:id,name:name},process.env.jwt_secretkey);
+}
+
+
+
+
+exports.userlogin = async(req,res,next)=>{
+    const{email,password} = req.body;
+    try{
+  const UserPresent=await Users.findOne({where:{email:email}});
+
+  if(UserPresent){
+    bcrypt.compare(password,UserPresent.password,(err,result)=>{
+        if(err)
+        {
+            throw new Error('Something Went Wrong');
+        }
+        if(result==true)
+        {
+            
+            const token = exports.generateAccessToken(UserPresent.id, UserPresent.name); 
+            return res.status(200).json({ message: 'Login Successfully', token: token });
+          }else{
+            return res.status(401).json({error:'Incorrect Password'});
+        }
+    })
+   
+}else{
+    return res.status(404).json({error:'Email does not exist'});
+    
+}
+
+
+
+
+
+
+    }catch(err){
+        console.log(err);
+        res.status(500).json({ message: 'An error occurred', error: err });
+     }
 }
--- routes/user.js
+++ routes/user.js
@@ -3,5 +3,6 @@ const  router = express.Router();
 const controler = require('../controller/user');
 
 router.post('/signup',controler.addUsers);
+router.post('/login',controler.userlogin);
 
 module.exports = router;
